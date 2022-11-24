b3c5a2c16b1753de83dc6fd4c9a3db4e7d1e516d
DanielaRomero <daniela.romero@epn.edu.ec>
2022-11-24 18:03:36 -0500

    Callbacks y ejercicio

---
+++ 01-javascript/06-callbacks.js
@@ -0,0 +1,41 @@
+// 06-callbacks
+const fs = require('fs');   // file system
+                            //Importar modulo fs
+
+// 1) Leer archivo:06-ejemplo.txt,
+// 2) Después del paso 1, Leer archivo: 01-variables.js
+//  , luego imprimir en consola
+// 3) Crear un nuevo archivo llamado 06-nuevo-archivo.txt
+//  con el contenido de los dos archivos
+
+
+fs.readFile(
+    './06-ejemplo.txt', //Nombre o path del archivo
+    'utf-8', //codificacion
+    (errorLecturaPrimerArchivo, contenidoPrimerArchivo) =>{
+        if(errorLecturaPrimerArchivo){
+            console.error('ERROR LEYENDO ARCHIVO', errorLecturaPrimerArchivo);
+        }else{
+            fs.readFile(
+                './01-javascript.js', //Nombre o path del archivo
+                'utf-8', //codificacion
+                (errorLecturaPrimerArchivo, contenidoSegundoArchivo) =>{
+                    if(errorLecturaPrimerArchivo){
+                        console.error('ERROR LEYENDO ARCHIVO', errorLecturaPrimerArchivo);
+                    }else{
+                        fs.writeFileSync(
+                            './06-nuevo-archivo.txt',
+                            contenidoPrimerArchivo+ "\n" + contenidoSegundoArchivo,
+                            (errorEscritura) =>{
+                                if(errorEscritura){
+                                    console.error('ERROR ESCRIBIENDO ARCHIVO: ', errorEscritura);
+                                }
+                            }
+                        );
+                    }
+                }
+            );
+
+        }
+    }
+);
