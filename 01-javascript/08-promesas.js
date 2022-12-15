9bdc1e5cf75b8c1c6a1e2c2931ad86e97a02fa22
Daniela Romero <daniela.romero@epn.edu.ec>
2022-12-15 17:54:35 -0500

    Sync Await Inquierer

--- 01-javascript/08-promesas.js
+++ 01-javascript/08-promesas.js
@@ -8,16 +8,81 @@ const fs = require('fs');
 * */
 
 function leerArchivo(path){
-    return new Promise(); //
+    return new Promise(
+        (resolve, reject)=>{
+            fs.readFile(
+                path, // Nombre o path del archivo
+                'utf-8', // codificacion
+                (errorLectura, contenido) => {
+                    if (errorLectura) {
+                        console.error(errorLectura);
+                        reject('Error leyendo primer archivo');
+                    } else {
+                        resolve(contenido)
+                    }
+                }
+            );
+        }
+    ); //
 }
-function escribirArchivo(path){
-    return new Promise(); //
+function escribirArchivo(path, nuevoContenido){
+    return new Promise(
+        (resolve, reject) =>{
+            fs.writeFile(
+                path,
+                nuevoContenido,
+                (errorEscritura) => {
+                    if (errorEscritura) {
+                        console.error(errorEscritura);
+                        reject('Error escribiendo nuevo archivo');
+                    } else {
+                        resolve('Completado');
+                    }
+                }
+            );
+        }
+    ); //
 }
 //
-function ejercicio08(path, contenidoArchivo){
-
+function ejercicio08(path, contenidoNuevo){
+    leerArchivo(path)
+        .then(
+            (contenidoArchivoOriginal)=>{
+                return escribirArchivo(
+                    path, contenidoArchivoOriginal + contenidoNuevo
+                )
+            }
+        )
+        .then()
+        .catch((error)=> console.error(error));
 }
 
-ejercicio08('06-ejemplo.txt', ' :)  lo logramos!')
-    .then()
-    .catch()
+//ejercicio08('06-ejemplo.txt', ' :)  lo logramos!');
+
+//SYNC AWAIT
+// Reglas
+// 1. Estar dentro de una funcion (nombrada o anonima)
+// 2. AGREGAR la palabra 'async' antes de la declaración de la función
+// 3. AGREGAR la palabra 'await' antes de la declaración de una promesa
+
+async function asyncAwaitUno(path, nuevoContenido){
+    // Si sabemos que en la promesa puede existir un reject usamos try-catch
+    try{
+        const respuestaContenidoArchivoOriginal = await leerArchivo(path);
+        await escribirArchivo(path, respuestaContenidoArchivoOriginal + nuevoContenido);
+        1 + 1; //caramelo
+        leerArchivo().then().catch()//async
+        await leerArchivo() //sync
+        // await escribirArchivo(path,(await leerArchivo(path)) + nuevoContenido);
+    }catch (error){
+        console.error(error)
+    }
+
+}
+asyncAwaitUno('06-ejemplo.txt', ' :)  lo logramos!').then().catch()
+const asyncAwaitDos = function (){
+
+}
+const asyncAwaitTres = () =>{
+
+}
---
+++ 01-javascript/proyecto-node/09-inquirer.js
@@ -0,0 +1,8 @@
+// 09-inquierer.js
+// npm init -> package.json -> dependencias ->scripts
+// npm install -> npm i inquirer
+// node_modules -> estan las dependencias
+const inquirer = require('inquirer');
+async function main(){
+
+}
