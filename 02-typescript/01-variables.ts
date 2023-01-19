e59e187cdf99daca54bb0616154a5b07fd677edb
DanielaRomero <daniela.romero@epn.edu.ec>
2023-01-18 20:38:23 -0500

    Duck Typing, Interfaces, Funciones, y Cases en TypeScript

--- 02-typescript/01-variables.ts
+++ 02-typescript/01-variables.ts
@@ -15,3 +15,36 @@ let casado: boolean = false;
 let fecha: Date = new Date();
 let sueldo: number;
 sueldo = 12.4;
+
+// Duck Typing
+let apellido = 'Eguez'; // string ->
+// apellido = 1; // No permite igualarle a otro tipo de datos
+apellido = 'Sarzosa';
+apellido.toUpperCase();
+
+// Si se quiere poner una variable que sea cualquier cosa se pone any
+let marihuana: any = 2;
+marihuana = '2';
+marihuana = true;
+marihuana = () => '2';
+
+// Una variable puede ser de multiples tipos de datos
+let edadMultiple: number | string |Date = '2'; // 2 | NewDate()
+edadMultiple = 2;
+edadMultiple = 'dos';
+edadMultiple = new Date();
+edadMultiple = 2222;
+
+// Utilizar edadMultiple para sumar otro numero
+// Para utilizar como numero edad multiple se debe castear
+let numeroUnico: number = 1;
+numeroUnico = numeroUnico + (edadMultiple as number);
+
+
+
+
+
+
+
+
+
---
+++ 02-typescript/02-interfaces.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.A = void 0;
+// 02-interfaces
+var A = /** @class */ (function () {
+    function A() {
+    }
+    return A;
+}());
+exports.A = A;
