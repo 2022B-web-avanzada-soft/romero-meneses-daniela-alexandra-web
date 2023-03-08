db7f3273c22f802834f4cfdb00fe399db8f0f317
DanielaRomero <daniela.romero@epn.edu.ec>
2023-03-08 11:21:16 -0500

    Control Forms

--- react-nextjs/pages/h_react_hook_form.tsx
+++ react-nextjs/pages/h_react_hook_form.tsx
@@ -1,18 +1,20 @@
 // react_hook_form
 import {useState} from "react";
-import {useForm} from "react-hook-form";
-import {Button} from "@mui/material";
+import {Controller, useForm} from "react-hook-form";
+import {Button, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 import Layout from "../components/Layout";
 
 type FormularioEjemplo = {
     nombre: string;
+    estadoCivil: string;
 }
 export default function (){
     const [nombre, setNombre] = useState('Daniela');
-    const {handleSubmit} = useForm<FormularioEjemplo>(
+    const {handleSubmit, register, formState:{errors, isValid}, control} = useForm<FormularioEjemplo>(
         {
             defaultValues:{
                 nombre: 'Alexandra',
+                estadoCivil: '',
             },
             mode: 'all'
         }
@@ -24,7 +26,83 @@ export default function (){
         <Layout title={'Formulario'}>
             <h1>Formulario con react Hook Form</h1>
             <form onSubmit={handleSubmit(controladorSubmit)}>
-                <Button type="submit" variant = 'outlined'>Submit</Button>
+                <div className="mb-3">
+                    <label htmlFor="nombre" className="form-label">Nombre</label>
+                    <input type="text"
+                           className="form-control"
+                           placeholder="EJ:Adrian"
+                           id="nombre"
+                           {...register('nombre',
+                               {
+                               required:
+                                   {
+                                   value: true,
+                                   message: 'nombre requerido'
+                                   },
+                                   maxLength: {value:20, message:'Longitud máxima 20'},
+                                   minLength: {value:5, message:'Longitud mínima 5'},
+                                   validate:{
+                                       soloNumeros:(valorActual)=>{
+                                           //transformar a numero un string
+                                           //Number("1")
+                                           //+"1"
+                                           if(Number.isNaN(+valorActual)){
+                                               //Se puede devolver un false a un mensaje de error
+                                               //return false; //Error
+                                               return 'Ingrese solo numeros';
+                                           } else{
+                                               return true;
+                                           }
+                                       }
+                                   }
+                               }
+                           )}
+                           aria-describedby="nombreHelp"
+                    />
+                    <div id="nombreHelp" className="form-text">
+                        Ingresa tu nombre.
+                    </div>
+                    {errors.nombre &&
+                        <div className="alert alert-warning"
+                             role="alert">
+                            Tiene errores {errors.nombre.message}
+                        </div>
+                    }
+                </div>
+                <div className= 'mb-3'>
+                    <FormControl fullWidth>
+                        <InputLabel id={"estadoCivilLabelId"}>Estado Civil</InputLabel>
+                        <Controller
+                            control={control}
+                            rules={{required:{value:true, message:'Estado C. requerido'}}}
+                            name="estadoCivil"
+                            render={
+                                ({field:{onChange, value,onBlur}})=>{
+                                    return <Select
+                                        labelId="estadoCivilLabelId"
+                                        id="estadoCivilId"
+                                        label="Estado Civil"
+                                        onBlur={onBlur}
+                                        value={value}
+                                        onChange={onChange}
+                                    >
+                                        <MenuItem value={'casado'}>Casado</MenuItem>
+                                        <MenuItem value={'soltero'}>Soltero</MenuItem>
+                                    </Select>
+                                }
+                            }
+                        />
+
+                        {/*Termina controller*/}
+                        {errors.estadoCivil &&
+                            <div className="alert alert-warning" role="alert">
+                                Tiene errores {errors.estadoCivil.message}
+                            </div>
+                        }
+                    </FormControl>
+                </div>
+
+                <Button type="submit" disabled={!isValid} variant = 'outlined'>Submit</Button>
             </form>
         </Layout>
         </>)
